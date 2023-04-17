fbb99f1e27d7c01faed490642b208d81a1d0b78e
Felipe Enmanuel Sandoval Sibada <felipesandoval1111@gmail.com>
2023-04-18 00:22:06 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -29,7 +29,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1g25-8Uc_iJ6ERTYTpjAembENkcp2hIF2/view?usp=share_link', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python3',
